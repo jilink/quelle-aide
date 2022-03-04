323724912dfc9dfb814cffa08feba0e9ee129a01
jilink <jimmyfac@gmail.com>
2022-03-04 11:50:29 +0100

    Button

--- app/components/ui/Button.js
+++ app/components/ui/Button.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import {Button as ChakraButton} from "@chakra-ui/react"
+import React from "react";
+import { Button as ChakraButton } from "@chakra-ui/react";
 
-const Button = ({children, ...props}) => {
+const Button = ({ children, ...props }) => {
   return (
-    <ChakraButton {...props}>{children}</ChakraButton>
-  )
-}
+    <ChakraButton
+      borderRadius="25px"
+      border="1px solid black"
+      boxShadow="dark-lg"
+      bg="white"
+      color="sunny"
+      {...props}
+    >
+      {children}
+    </ChakraButton>
+  );
+};
 
-export default Button
+export default Button;
